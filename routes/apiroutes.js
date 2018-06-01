1213f5a4f3e3723be38514296179eb3cb784ef18
John Firth <johfirth@gmail.com>
2018-05-31 21:06:47 -0400

    more api path crap

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -1,16 +1,27 @@
 const path = require('path');
+const db = require ('../SEQUELIZESTUFF')
 
 
 
 
 module.exports = function(app) {
-  app.get('/api/buy', function(req, res){
-    res.json(item);
+  app.get('/api/posts', function(req, res){
+    var query = {};
+    if (req.query.seller_id) {
+      query.SellerID = req.query.seller_id
+    }
+    db.Post.findAll({
+      where: query,
+      include: [db.Product]
+    }).then(function(dbPost){
+      res.json(dbPost)
+    });
 
   });
 
-  app.post('/api/sell', function (req, res){
-    friendName.push(req.body);
-    res.json(true)
-  })
+  app.post('/api/posts', function (req, res){
+    db.Post.create(req.body).then(dbPost => {
+      res.json(dbPost)
+    });
+  });
 }
