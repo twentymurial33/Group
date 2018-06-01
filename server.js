c70057b0c653c62b1dd162bdbd7021d16d804cb9
Murial Anindo <twentymurial@outlook.com>
2018-05-31 20:51:27 -0400

    second

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 var express = require("express");
 var bodyParser = require("body-parser");
-var sequelize = require("sequilize");
+var mysql=require("mysql");
+var path=require("path");
+// View Engine
+app.set("view engine","ejs");
+app.set("views",path.join(__dirname,"views"));
+
+app.get("/",function(req,res){
+res.render("index");});
 
 var app = express();
 var PORT = process.env.PORT || 3000;
