11f12a3325488809289411ad8c10a5f41a672387
Murial Anindo <twentymurial@outlook.com>
2018-06-02 11:19:58 -0400

    changes to server.js

--- server.js
+++ server.js
@@ -1,19 +1,30 @@
 var express = require("express");
 var bodyParser = require("body-parser");
-//Middleware
-const app = express();
-// View Engine
+var PORT = process.env.PORT || 3000;
+var path=require("path");
+
+var app = express();
+
+app.use(express.static("public"));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(bodyParser.json());
+
+// let ejs = require('ejs');
+
 app.set("view engine","ejs");
-app.set("views",path.join(__dirname,"views"));
+// app.engine("ejj", );
 
-// Static Path
-app.use(express.static(path.join(__dirname,"public")))
+// var routes = require("/routes/apiroutes.js");
 
-app.get("/",function(req,res){
-res.render("index");});
+// app.use(routes);
+// app.get("/",function(req,res){
+// res.render("index");});
 
+// ejs.render('<$= users.join(" | "); $>', {users: users});
+// => 'geddy | neil | alex'
 
-
-app.listen(3000,function(){
+app.listen(PORT,function(){
    console.log("Server Started on Port 3000");
 })
