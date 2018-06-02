0741e52b01e792aa4fb221a3f72fc05342d9e409
John <johfirth@gmail.com>
2018-06-02 11:31:55 -0400

    fix server issue

--- server.js
+++ server.js
@@ -5,19 +5,19 @@ var path=require("path");
 
 var app = express();
 
-app.use(express.static("public"));
+
 
 app.use(bodyParser.urlencoded({ extended: true }));
-
 app.use(bodyParser.json());
+app.use(express.static("public"));
 
 // let ejs = require('ejs');
 
 app.set("view engine","ejs");
 // app.engine("ejj", );
 
-// var routes = require("/routes/apiroutes.js");
-
+require('./routes/apiroutes.js')(app);
+require('./routes/htmlroutes.js')(app);
 // app.use(routes);
 // app.get("/",function(req,res){
 // res.render("index");});
