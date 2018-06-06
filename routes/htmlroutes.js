7ffaf1fe5c727ec2f4161bb864dc0ceaa1be2589
Murial Anindo <twentymurial@outlook.com>
2018-06-06 16:58:02 -0400

    promise no more

--- routes/htmlroutes.js
+++ routes/htmlroutes.js
@@ -7,19 +7,18 @@ module.exports = function(app) {
     res.sendFile(path.join(__dirname, '../views/index.handlebars'))
   });
 
-  app.get('/seller', function(req, res){
-    res.sendFile(path.join(__dirname, '../views/seller.handlebars'))
-  });
+  // app.get('/seller', function(req, res){
+  //   res.sendFile(path.join(__dirname, '../views/seller.handlebars'))
+  // });
   app.get('/buy', function(req, res){
     res.sendFile(path.join(__dirname, '../views/buy.handlebars'))
   });
 
   app.get('/seller', function(req, res) {
-    res.render( 'seller', {variableName: 'someDataToDisplayOnPage'} );
-  
-  db.Product.findAll()
+    return( 'seller', {variableName: 'products'} );
+    db.Product.findAll()
   .then(function(product) {
-    res.render('seller.handlebars',newItem);
+    res.render('seller',newItem);
   });
 });
 }
