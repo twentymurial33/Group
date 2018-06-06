e4a871c4a64c8c96d5a92a5bf476492184188af4
Murial Anindo <twentymurial@outlook.com>
2018-06-06 18:07:51 -0400

    changes to index..added href and buy.handlebars

--- routes/htmlroutes.js
+++ routes/htmlroutes.js
@@ -10,9 +10,18 @@ module.exports = function(app) {
   // app.get('/seller', function(req, res){
   //   res.sendFile(path.join(__dirname, '../views/seller.handlebars'))
   // });
-  app.get('/buy', function(req, res){
-    res.sendFile(path.join(__dirname, '../views/buy.handlebars'))
+  // app.get('/buy', function(req, res){
+  //   res.sendFile(path.join(__dirname, '../views/buy.handlebars'))
+  // });
+
+  app.get('/buy', function(req, res) {
+    return( 'buy', {variableName: 'products'} );
+    db.Product.findAll()
+  .then(function(product) {
+    res.render('buy',newItem);
   });
+});
+}
 
   app.get('/seller', function(req, res) {
     return( 'seller', {variableName: 'products'} );
@@ -21,4 +30,3 @@ module.exports = function(app) {
     res.render('seller',newItem);
   });
 });
-}
