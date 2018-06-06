f4875644f1290f96e8c815f5283f90b6f65fd6d4
Adrienne Schueller <atschueller@gmail.com>
2018-06-05 20:37:38 -0400

    firebase crap

--- public/assets/js/yardsale.js
+++ public/assets/js/yardsale.js
@@ -5,34 +5,45 @@ var email = '';
 var password = '';
 var oldEmail = '';
 var oldPassword = '';
-
-var config = {
-    apiKey: "AIzaSyAz8V0oTOGh0If5LubLLMPGF8OQTZeWJ4U",
-    authDomain: "yard-sale-fe238.firebaseapp.com",
-    databaseURL: "https://yard-sale-fe238.firebaseio.com",
-    projectId: "yard-sale-fe238",
-    storageBucket: "yard-sale-fe238.appspot.com",
-    messagingSenderId: "1098564084379"
-};
-firebase.initializeApp(config);
-var database = firebase.database();
-
-function newAccount() {
-    email = $('#newemailInput').val();
-    password = $('#newpasswordInput').val();
-};
-function oldAccount() {
-    oldEmail = $('#oldEmail').val();
-    oldPassword = $('#oldPassword').val();
-};
-
 $(document).ready(function () {
-    console.log("file loaded!!!");
+    var config = {
+        apiKey: "AIzaSyAz8V0oTOGh0If5LubLLMPGF8OQTZeWJ4U",
+        authDomain: "yard-sale-fe238.firebaseapp.com",
+        databaseURL: "https://yard-sale-fe238.firebaseio.com",
+        projectId: "yard-sale-fe238",
+        storageBucket: "yard-sale-fe238.appspot.com",
+        messagingSenderId: "1098564084379"
+    };
+    firebase.initializeApp(config);
+    var database = firebase.database();
+
+    function newAccount() {
+        email = $('#newemailInput').val();
+        password = $('#newpasswordInput').val();
+    };
+    function oldAccount() {
+        oldEmail = $('#oldEmail').val();
+        oldPassword = $('#oldPassword').val();
+    };
+    function newUserData() {
+        newAccount();
+        database.ref().push({
+            email: email,
+            password: password,
+            dateAdded: firebase.database.ServerValue.TIMESTAMP
+        });
+        database.ref().on("child_added", function (childSnapshot) {
+            console.log(childSnapshot.val().email);
+            console.log(childSnapshot.val().password);
+        });
+    };
     $("#createAccount").on('click', function (event) {
         event.preventDefault();
         $('#new-user-modal').show();
         $('#signIn').hide();
         $('#createAccount').hide();
+    });
+    $('#signupUser').on('click', function(event) {
         newAccount(email, password);
         firebase.auth().createUserWithEmailAndPassword(email, password).catch(function (error) {
             var errorCode = error.code;
@@ -40,21 +51,20 @@ $(document).ready(function () {
             if (errorCode == 'auth/weak-password') {
                 console.log(error);
             };
+            newUserData();
         });
     });
-    $('#signIn').on('click', function (event) {
-        event.preventDefault();
-        $('#return-user-modal').show();
-        $('#signIn').hide();
-        $('#createAccount').hide();
-        oldAccount();
-        firebase.auth().signInWithEmailAndPassword(email, password).catch(function (error) {
-            var errorCode = error.code;
-            var errorMessage = error.message;
-        });
+$('#signIn').on('click', function (event) {
+    event.preventDefault();
+    $('#return-user-modal').show();
+    $('#signIn').hide();
+    $('#createAccount').hide();
+    oldAccount();
+    firebase.auth().signInWithEmailAndPassword(email, password).catch(function (error) {
+        var errorCode = error.code;
+        var errorMessage = error.message;
     });
 });
-
 function addItem(event) {
     //event.preventDefault();
     var itemInput = $('#item-name').val().trim();
@@ -76,13 +86,11 @@ function addItem(event) {
     $('#item-description').empty();
     $('#item-url').empty();
 };
-
 $('#uploadBtn').on('click', function (event) {
     event.preventDefault();
     console.log("button clicked!!");
     addItem();
-
 });
-
+});
 
 
